37de5eaa57b7de6ce14375490fd92528b393f2ad
Toloaca Ion <i.toloaca@jacobs-university.de>
2015-11-07 00:32:47 +0100

    User action

---
+++ php/create_user.php
@@ -0,0 +1,31 @@
+<html>
+<body>
+<?php
+
+$password = $_POST['password'];
+if (strlen($password) < 5) {
+	die('The password was invalid. Better luck next time.');
+}
+$password_hash = sha1($_POST['password']);
+echo $password;
+echo $password_hash;
+$con = mysql_connect("localhost:3306","dbwa_sparta","foobar33");
+if (!$con) {
+  die('Could not connect: ' . mysql_error());
+}
+ 
+mysql_select_db("dbwa_sparta", $con);
+
+
+$sql = "INSERT INTO User (name, email, password_hash)
+VALUES ('$_POST[name]','$_POST[email]', '$password_hash')";
+ 
+if (!mysql_query($sql, $con)) {
+  die('Error: ' . mysql_error());
+}
+echo "1 user added";
+ 
+mysql_close($con);
+?>
+</body>
+</html>
